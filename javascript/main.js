a9ac824b8b06edd81d6c84aa51c9b977d4c04ece
nreyni <urwatulwuska282@gmail.com>
2023-07-28 11:21:27 +0700

    mengantikan nama file

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.btnDetail').forEach(item=> {
+    item.addEventListener('click', (e) => {
+        let parent = e.target.parentNode.parentNode
+        let gambar = parent.querySelector('.card-img-top').src
+        let harga = parent.querySelector('.harga').innerHTML
+        let judul = parent.querySelector('.card-text').innerHTML
+        let deskripsi = parent.querySelector('.deskripsi')? parent.querySelector('.deskripsi').innerHTML:'tidak ada informasi yang tersedia'
+        
+        let tombolModal = document.querySelector('.btnModal')
+        tombolModal.click()
+
+        document.querySelector('.modalTitle').innerHTML = judul
+        let image = document.createElement('img')
+        image.src = gambar
+        image.classList.add('w-100')
+        document.querySelector('.modalImage').innerHTML = ''
+        document.querySelector('.modalImage').appendChild(image) = image
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi
+        document.querySelector('.modalHarga').innerHTML = harga
+
+        const noHp = '6281350554300'
+        let pesan = `https//api.whatsapp.com/send?phone=${noHp}&=Halo bg saya mau pesan prodak ini ${gambar}`
+        document.querySelector('.btnBeli').herf = pesan
+    })
+})
